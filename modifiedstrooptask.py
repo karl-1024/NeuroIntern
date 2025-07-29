90f8889d3c58644f4ad7d5731534b78bd458d10a
karl-1024 <karlchujh@gmail.com>
2025-07-29 20:22:55 +0800

    finishing touches.

--- modifiedstrooptask.py
+++ modifiedstrooptask.py
@@ -4,18 +4,38 @@ import pandas as pd
 
 #assessments = [fixation_on, fixation_off, stimuli_on, stimuli_off, reaction_speed]
 experiment_clock = core.Clock()
-def display_instructions():
-    instruction_text = " " 
-    instructions = visual.TextStim(win, text = instruction_text)
 win = visual.Window(size=(800,600), color='grey', units='pix')
+def display_instructions():
+    instruction_text = ("在這個遊戲中，您將看到顏色詞語（紅色，藍色，綠色）逐一出現。\n\n"
+                        "您的任務是按下與字體顏色相對應的按鈕，而不是詞語本身。\n\n"
+                        "儘量快速和準確地回應。\n\n"
+                        "回應鍵如下:\n\n"
+                        "r = 紅色\n"
+                        "b = 藍色\n"
+                        "g = 綠色\n\n"
+                        "記住，選擇字母的顏色，忽略詞語本身。\n\n"
+                        "按 'Enter' 開始，一旦您理解了規則。")
+    #displays instruction text 
+    instructions = visual.TextStim(win, text = instruction_text, pos = (-5.0,0.0))
+    instructions.draw()
+    win.flip()  
+    enter_key = event.waitKeys(keyList = ['return'])
+    #waits for response (enter key) 
+    if enter_key: 
+        finger_placement = visual.ImageStim(win, image = 'finger_instruction.png')
+        finger_placement.draw() 
+        win.flip()
+        core.wait(3)
+    
+
 color_translations = {"red" : "紅", "blue" :"藍", "green" : "綠", "brown" : "棕"}
 color_chinese = ["紅", "藍", "綠"]
 color_english = ["red", "blue", "green"]
 experiment_data = []
 
 pairings = []
-for i in color_chinese: 
-    for j in color_english: 
+for i in color_english: 
+    for j in color_chinese: 
         pairings.append((i,j))
 random.shuffle(pairings) 
 def display_stimuli():
@@ -32,8 +52,12 @@ def display_stimuli():
         fixation_offset = experiment_clock.getTime() 
         
         #display stimuli 
-        correct_color = random.choice(color_english) 
-        test = visual.TextStim(win, text = random.choice(color_chinese), height = 60, color = correct_color) 
+        #test_text is tuple -- (english, chinese) 
+        test_text = pairings[i]
+        correct_color = test_text[0]
+        #checks if color of text is same as text 
+        congruence = color_translations[correct_color] == test_text[1]
+        test = visual.TextStim(win, text = test_text[1], height = 60, color = correct_color) 
         test.draw()
         win.flip(clearBuffer = False)
         stimulus_onset = experiment_clock.getTime()
@@ -42,20 +66,27 @@ def display_stimuli():
         if response: 
             data = {
                     "response_key" : response[0][0], 
-                    "correct" : response[0][0] == correct_color 
-                    "fixation_onset" : fixation_onset
-                    "fixation_offset" : fixation_offset 
+                    "correct_color" : correct_color,
+                    "correct" : response[0][0] == correct_color[0] ,
+                    "congruent" : congruence,
+                    "fixation_onset" : fixation_onset,
+                    "fixation_offset" : fixation_offset ,
                     "stimulus_onset": stimulus_onset,
-                    "response_time" : response[0][1], 
+                    "response_time" : response[0][1],
+                    "response_speed" : response[0][1] - stimulus_onset
                     }
             experiment_data.append(data)
                     
             
-            
+    
         
 
 experiment_clock.reset() 
+display_instructions()
 display_stimuli()
+print(experiment_data)
+print(pairings)
+
 
 
 
